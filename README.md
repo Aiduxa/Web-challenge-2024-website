f04a3938cfbfddc5b3435b91cd71bdb6d358d7b1
WEB Contest <contest@kitm.local>
2024-03-28 10:55:05 +0200

    first initialization

---
+++ README.md
@@ -0,0 +1,13 @@
+# Web challenge 2024 entry 
+
+This is a repo for a AI integrated app.
+
+# How to install?
+
+Move into your **./website** directory
+` cd website `
+
+Install required packages.
+` npm install `
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.jsx",
+    "./src/**/*.css", 
+    "./src/**/*.html", 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
